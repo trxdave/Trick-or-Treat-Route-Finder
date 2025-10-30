fef8f302b9400d090d60f2098023b76b7781de1d
Mulloo <mulloolyd.1998@gmail.com>
2025-10-30 12:05:47 +0000

    started dan branch

--- mapapp/models.py
+++ mapapp/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+
 
 # Create your models here.
+class House (models.Model):
+    STATUS_CHOICES = [
+        ('giving', 'Giving Out Candy'),
+        ('out', 'Out of Candy'),
+        ('none', 'Not Participating'),
+    ]
+    address = models.CharField(max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='none')
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return f"{self.address} - {self.get_status_display()}"
