03eb71bdbbaddc6eb8a575f8feec42ab58195593
David Cotter <daveguy28@gmail.com>
2025-11-01 13:35:04 +0000

    team page is up

--- mapapp/urls.py
+++ mapapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
 
     path('', views.home, name='home'),
+    path("team/", views.team, name="team"),
     path('api/houses/', views.houses_json, name='houses_json'),
     path('add/', views.add_house, name='add_house'),
     path('api/houses/<int:pk>/delete/', views.delete_house, name='delete_house'),
