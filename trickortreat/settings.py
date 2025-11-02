f95fd0f42903c563a09c196b5278cd9d4251b67b
David Cotter <daveguy28@gmail.com>
2025-11-02 17:05:23 +0000

    debug to false

--- trickortreat/settings.py
+++ trickortreat/settings.py
@@ -20,7 +20,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
+DEBUG = False
 
 TEMPLATES_DIR = BASE_DIR / "templates"
 STATIC_URL = "static/"
